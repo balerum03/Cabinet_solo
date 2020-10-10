8db87f35b720850abff8420daf42527c39e27889
balerum03 <adafer123.angra@gmail.com>
2020-10-09 23:31:29 -0500

    edit and destroy were created

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -27,9 +27,16 @@ class DocsController < ApplicationController
   end
 
   def update
+    if @doc.update(doc_params)
+        redirect_to @doc
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @doc.destroy
+    redirect_to doc_path
   end
 
   private
